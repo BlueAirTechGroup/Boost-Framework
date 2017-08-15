8d4c5fc9850a864a6b366a9878b0d49dc065265b
ToiletCommender <cxcyhdyx@sina.com>
2017-08-14 21:48:55 -0400

    Initial commit

---
+++ js/METERIOT.main.js
@@ -0,0 +1,146 @@
+var loadingWrapperDeleted;
+loadingWrapperDeleted=false;
+
+document.onreadystatechange = completeLoading;
+
+
+	var prompttime = 0;
+	var promptx = [];
+	var prompty = [];
+	var promptdraging = [];
+    var browser = function () {   
+    var agent = navigator.userAgent.toLowerCase(),  
+    opera = window.opera,  
+    browser = {  
+        //检测当前浏览器是否为IE  
+        ie: /(msie\s|trident.*rv:)([\w.]+)/.test(agent), 
+ 
+        //检测当前浏览器是否为Opera  
+        opera: (!!opera && opera.version), 
+        //检测当前浏览器是否是webkit内核的浏览器  
+        webkit: (agent.indexOf(' applewebkit/') > -1), 
+        //检测当前浏览器是否是运行在mac平台下  
+        mac: (agent.indexOf('macintosh') > -1), 
+        //检测当前浏览器是否处于“怪异模式”下  
+        quirks: (document.compatMode == 'BackCompat')  
+    }; 
+    //检测当前浏览器内核是否是gecko内核  
+    browser.gecko = (navigator.product == 'Gecko' && !browser.webkit && !browser.opera && !browser.ie); 
+    var version = 0; 
+    // Internet Explorer 6.0+  
+    if (browser.ie) {  
+        var v1 = agent.match(/(?:msie\s([\w.]+))/);  
+        var v2 = agent.match(/(?:trident.*rv:([\w.]+))/);  
+        if (v1 && v2 && v1[1] && v2[1]) {  
+            version = Math.max(v1[1] * 1, v2[1] * 1);  
+        } else if (v1 && v1[1]) {  
+            version = v1[1] * 1;  
+        } else if (v2 && v2[1]) {  
+            version = v2[1] * 1;  
+        } else {  
+            version = 0;  
+        } 
+        //检测浏览器模式是否为 IE11 兼容模式  
+        browser.ie11Compat = document.documentMode == 11; 
+        //检测浏览器模式是否为 IE9 兼容模式  
+        browser.ie9Compat = document.documentMode == 9; 
+        //检测浏览器模式是否为 IE10 兼容模式  
+        browser.ie10Compat = document.documentMode == 10; 
+        //检测浏览器是否是IE8浏览器  
+        browser.ie8 = !!document.documentMode; 
+        //检测浏览器模式是否为 IE8 兼容模式  
+        browser.ie8Compat = document.documentMode == 8; 
+        //检测浏览器模式是否为 IE7 兼容模式  
+        browser.ie7Compat = ((version == 7 && !document.documentMode) || document.documentMode == 7); 
+        //检测浏览器模式是否为 IE6 模式 或者怪异模式  
+        browser.ie6Compat = (version < 7 || browser.quirks); 
+        browser.ie9above = version > 8; 
+        browser.ie9below = version < 9;  
+    } 
+    // Gecko.  
+    if (browser.gecko) {  
+        var geckoRelease = agent.match(/rv:([\d\.]+)/);  
+        if (geckoRelease) {  
+            geckoRelease = geckoRelease[1].split('.');  
+            version = geckoRelease[0] * 10000 + (geckoRelease[1] || 0) * 100 + (geckoRelease[2] || 0) * 1;  
+        }  
+    } 
+    //检测当前浏览器是否为Chrome, 如果是，则返回Chrome的大版本号  
+    if (/chrome\/(\d+\.\d)/i.test(agent)) {  
+        browser.chrome = +RegExp['\x241'];  
+    } 
+    //检测当前浏览器是否为Safari, 如果是，则返回Safari的大版本号  
+    if (/(\d+\.\d)?(?:\.\d)?\s+safari\/?(\d+\.\d+)?/i.test(agent) && !/chrome/i.test(agent)) {  
+        browser.safari = +(RegExp['\x241'] || RegExp['\x242']);
+    } 
+    // Opera 9.50+  
+    if (browser.opera){ 
+        version = parseFloat(opera.version()); 
+	}
+    // WebKit 522+ (Safari 3+)  
+    if (browser.webkit){
+        version = parseFloat(agent.match(/ applewebkit\/(\d+)/)[1]); 
+	}
+    //检测当前浏览器版本号  
+    browser.version = version; 
+    return browser;  
+}();
+$(window).ready(
+function(e){
+	"use strict";
+	var ScreenWidth = parseInt(GetWindowWidth());
+	var ScreenHeight = parseInt(GetWindowHeight());
+	if(loadingWrapperDeleted==false){
+		$("body").append('<div id="loadingWrapper" class="cover bg-white" style="position:fixed;top:0px;left:0px;width:100vw;height:100vw;z-index:9999;display:table;vertical-align:middle;"><div class="inner" style="display:table-cell"><!-- [if gt IE 8]><p align="center"><div class="boost-animate-spinner"></div></p><p align="center">Powered by Boost Framework</p><![endif]--><!--[if lt IE 9]><p>当前网页<b>不支持</b>您正在使用的浏览器. 为了正常的访问,请升级您的浏览器</p><p>Sorry, this page does not support your current browser, to change this situation, you will need to update your browser</p><![endif]--></div></div>')
+	}
+	if(ScreenWidth<1201){
+		//Phone or tablet
+		$(".navbar-links").hide();
+	}
+	$("navicon").click(
+	function(e){
+		var MyContainer = $(e.target).parent(".container");
+		var MyLinks = MyContainer.children(".navbar-links");
+		MyLinks.toggle();
+	}
+	);
+}
+);
+$(window).resize(
+function(e){
+	"use strict";
+	var ScreenWidth = parseInt(GetWindowWidth());
+	var ScreenHeight = parseInt(GetWindowHeight());
+	if(ScreenWidth<1201){
+		//Phone or tablet
+		$(".navbar-links").hide();
+	}else{
+		$(".navbar-links").show();
+	}
+}
+);
+
+function GetWindowWidth(){
+	"use strict";
+	return $(window).width();
+}
+function GetWindowHeight(){
+	"use strict";
+	return $(window).height();
+}
+function completeLoading(){
+	if(document.readyState == "complete"){
+		loadingWrapperDeleted=true;
+		//做爱做的事
+		$("#LoadingWrapper").hide();
+		$("#LoadingWrapper").remove();
+	}
+}
+
+if(browser.ie9below === true){
+		//不符合浏览器要求(因为不支持该版本Jquery)
+		//提示, 你需要更换GFW出卖引擎
+		document.write(
+			'<div class="cover bg-white" style="width:100%;height:100%;width:100vw;height:100vh;position:fixed;top:0px;left:0px;text-align:center;z-index:9999;"><h1>对不起, 您的浏览器不支持此网页</h1><p>Sorry, your browser does not support this page</p><p>Powered by Boost Framework</p></div>';
+		)
+}
